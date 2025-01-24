5f87071c625a2204f6c02e423ffefced7c2d619e
R.Andres Castaneda Aguilar <acastanedaa@worldbank.org>
2025-01-24 17:40:28 -0500

    init vignette

---
+++ R/piptest-package.R
@@ -0,0 +1,10 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @rawNamespace import(collapse, except = fdroplevels)
+#' @rawNamespace import(data.table, except = fdroplevels)
+#' @import pipfun
+## usethis namespace: end
+NULL
+
